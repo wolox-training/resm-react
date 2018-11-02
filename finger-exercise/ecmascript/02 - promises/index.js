63944805b81ad4b7bf15b4e4aa64d2242723642b
Raul Suescun <raulsuescun@gmail.com>
2018-11-02 15:55:06 -0500

    Promises: test01 - delay works well

--- finger-exercise/ecmascript/02 - promises/index.js
+++ finger-exercise/ecmascript/02 - promises/index.js
@@ -1,7 +1,17 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const timeStart = Date.now();
+  let delayedTime = 0;
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      delayedTime = Date.now() - timeStart;
+      if (delayedTime >= (time - 100) && delayedTime <= (time + 100)) {
+        clearInterval(interval);
+        resolve(delayedTime);
+      }
+    }, 100);
+  });
 }
 
 export function asyncDelay() {
