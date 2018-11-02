b80592a93d0a9e344745e7dad3d5962d1d37aaca
Raul Suescun <raulsuescun@gmail.com>
2018-11-02 09:09:35 -0500

    Spread operator: test01 - min returns the param if is the only argument

--- finger-exercise/ecmascript/01 - spread/index.js
+++ finger-exercise/ecmascript/01 - spread/index.js
@@ -1,8 +1,11 @@
-import { isArray } from './utils';
+import isArray from './utils';
 
-export function min() {
-  isArray();
-  return Math.min();
+export function min(...args) {
+  if (!isArray(args) && args.length === 1) {
+    const [param] = [...args];
+    return param;
+  }
+  return Math.min(...args);
 }
 
 export function copy() {
