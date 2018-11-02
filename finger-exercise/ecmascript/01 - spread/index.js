cc52842f29254e81ce64e1cbc6b2c23c93d6e228
Raul Suescun <raulsuescun@gmail.com>
2018-11-02 09:56:27 -0500

    Spread operator: test02 - min can return the minimum based on a list

--- finger-exercise/ecmascript/01 - spread/index.js
+++ finger-exercise/ecmascript/01 - spread/index.js
@@ -3,6 +3,9 @@ import isArray from './utils';
 export function min(...args) {
   if (isArray(args) && args.length === 1) {
     const [param] = [...args];
+    if (isArray(param)) {
+      return Math.min(...param);
+    }
     return param;
   }
   return Math.min(...args);
