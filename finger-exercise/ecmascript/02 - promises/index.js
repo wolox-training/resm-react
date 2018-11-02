cdfa731c4f1e492edce7ebe426eaba3c8a802a8d
Raul Suescun <raulsuescun@gmail.com>
2018-11-02 16:02:41 -0500

    Promises: test02 - delay will not wait if the time is too long

--- finger-exercise/ecmascript/02 - promises/index.js
+++ finger-exercise/ecmascript/02 - promises/index.js
@@ -3,12 +3,15 @@
 export function delay(time) {
   const timeStart = Date.now();
   let delayedTime = 0;
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const interval = setInterval(() => {
       delayedTime = Date.now() - timeStart;
       if (delayedTime >= (time - 100) && delayedTime <= (time + 100)) {
         clearInterval(interval);
         resolve(delayedTime);
+      } else if (delayedTime >= (4900)) {
+        const reason = new Error('This time is too much !');
+        reject(reason);
       }
     }, 100);
   });
