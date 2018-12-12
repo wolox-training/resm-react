6b52385d1e747ec10b838d3e368693567ef53f8e
Raul Suescun <raulsuescun@gmail.com>
2018-12-12 13:01:38 -0500

    02 - async - getData throws error if false ...  async/await

--- finger-exercise/testing_1/02 - async/test.js
+++ finger-exercise/testing_1/02 - async/test.js
@@ -2,15 +2,23 @@ import getData from '.';
 
 describe('02 - async', () => {
   it('getData resolves the data if true is sent as argument (use async/await)', async () => {
+    expect.assertions(1);
     const data = await getData(true);
     expect(data).toBe('data');
   });
   it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+    expect.assertions(1);
     return getData(true).then(data => {
       expect(data).toBe('data');
     });
   });
-  xit('getData throws error if false is sent as argument (use async/await)', async () => {
+  it('getData throws error if false is sent as argument (use async/await)', async () => {
+    expect.assertions(1);
+    try {
+      await getData(false);
+    } catch (e) {
+      expect(e).toEqual(new Error('error'));
+    }
   });
   xit('getData throws error if false is sent as argument (avoid async/await)', () => {
   });
