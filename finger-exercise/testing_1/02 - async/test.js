daa9b65a4bed57c85e0be8300bba6ce50a6fcaba
Raul Suescun <raulsuescun@gmail.com>
2018-12-12 12:11:49 -0500

    02 - async - getData resolves the data if true ...  (avoid async/await)

--- finger-exercise/testing_1/02 - async/test.js
+++ finger-exercise/testing_1/02 - async/test.js
@@ -3,9 +3,12 @@ import getData from '.';
 describe('02 - async', () => {
   it('getData resolves the data if true is sent as argument (use async/await)', async () => {
     const data = await getData(true);
-    expect(data).toMatch('data');
+    expect(data).toBe('data');
   });
-  xit('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+  it('getData resolves the data if true is sent as argument (avoid async/await)', () => {
+    return getData(true).then(data => {
+      expect(data).toBe('data');
+    });
   });
   xit('getData throws error if false is sent as argument (use async/await)', async () => {
   });
