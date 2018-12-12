816caa6548efb74df781044a697fe67646fad7f1
Raul Suescun <raulsuescun@gmail.com>
2018-12-12 11:31:29 -0500

    01 - matchers - pow returns an array of power results ...

--- finger-exercise/testing_1/01 - matchers/test.js
+++ finger-exercise/testing_1/01 - matchers/test.js
@@ -15,7 +15,11 @@ describe('01 - matchers', () => {
     expect(pow(10)).toBeUndefined();
     expect(pow(900)).toBeUndefined();
   });
-  xit('pow returns an array of power results if array of pairs are sent as arguments', () => {
+  it('pow returns an array of power results if array of pairs are sent as arguments', () => {
+    expect(pow([9, 3], [-9, 3])).toEqual([729, -729]);
+    expect(pow([5, 3], [10, 5])).toEqual([125, 100000]);
+    expect(pow([5, 3], [10, 5], [7, 4])).toEqual([125, 100000, 2401]);
+    expect(pow([5, 3], [10, 5], [7, 4], [8, 8])).toEqual([125, 100000, 2401, 16777216]);
   });
   xit('pow returns undefined in the right position of the result array if pair is not as expected', () => {
   });
