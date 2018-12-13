48e0cd296a5504aebe3012925e9ea6df969662a3
Raul Suescun <raulsuescun@gmail.com>
2018-12-13 09:35:00 -0500

    UserList - shows new user when props are changed

--- finger-exercise/testing_2/src/screens/Dashboard/components/UserList/index.test.js
+++ finger-exercise/testing_2/src/screens/Dashboard/components/UserList/index.test.js
@@ -9,7 +9,17 @@ import UserList from './index';
 configure({ adapter: new Adapter() });
 
 describe('UserList', () => {
-  xit('shows new user when props are changed', () => {
+  it('shows new user when props are changed', () => {
+    const users = [
+      { id: 1, name: 'Jane', active: true },
+      { id: 2, name: 'John', active: true },
+      { id: 3, name: 'Sean', active: false }
+    ];
+    const wrapper = shallow(<UserList users={users} />);
+    expect(wrapper.find('UserRow').length).toBe(3);
+    users.push({ id: 4, name: 'Marta', active: true });
+    wrapper.setProps({ users });
+    expect(wrapper.find('UserRow').length).toBe(4);
   });
   xit('setProps makes componentDidUpdate to be executed', () => {
   });
