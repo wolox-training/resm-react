24df8fb3fab3b6ce11fe9fb69a80ccd826cef164
Raul Suescun <raulsuescun@gmail.com>
2018-12-13 09:40:05 -0500

    UserList - setProps makes componentDidUpdate to be executed

--- finger-exercise/testing_2/src/screens/Dashboard/components/UserList/index.test.js
+++ finger-exercise/testing_2/src/screens/Dashboard/components/UserList/index.test.js
@@ -21,6 +21,19 @@ describe('UserList', () => {
     wrapper.setProps({ users });
     expect(wrapper.find('UserRow').length).toBe(4);
   });
-  xit('setProps makes componentDidUpdate to be executed', () => {
+  it('setProps makes componentDidUpdate to be executed', () => {
+    jest.spyOn(UserList.prototype, 'componentDidUpdate');
+    const users = [
+      { id: 1, name: 'Jane', active: true },
+      { id: 2, name: 'John', active: true },
+      { id: 3, name: 'Sean', active: false }
+    ];
+    const wrapper = shallow(<UserList users={users} />);
+    users.push({ id: 4, name: 'Marta', active: true });
+    wrapper.setProps({ users });
+    expect(UserList.prototype.componentDidUpdate.mock.calls.length).toBe(1);
+    users.push({ id: 5, name: 'Alejandra', active: true });
+    wrapper.setProps({ users });
+    expect(UserList.prototype.componentDidUpdate.mock.calls.length).toBe(2);
   });
 });
