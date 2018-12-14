6a1a56be9e68bc712edab2b36927559e0d9abfce
Raul Suescun <raulsuescun@gmail.com>
2018-12-14 14:05:24 -0500

    Changed definition of the 'Rules' component

--- react-app/src/app/screens/User/components/Points/components/Rules/index.js
+++ react-app/src/app/screens/User/components/Points/components/Rules/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import { POINTS_RULES } from '../../../../../../constants';
 
-const Rules = () => (
-  <ul>
-    {POINTS_RULES.map((rule, i) => (
-      <li key={i.toString()}>
-        Obtain <b>{rule.points} points</b> winning with {rule.moves} moves
-      </li>
-    ))}
-  </ul>
-);
+function Rules() {
+  return (
+    <ul>
+      {POINTS_RULES.map((rule, i) => (
+        <li key={i.toString()}>
+          Obtain <b>{rule.points} points</b> winning with {rule.moves} moves
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default Rules;
