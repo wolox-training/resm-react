f9015d884004f1dd4fbb22cc8923a103d4c55c6c
Raul Suescun <raulsuescun@gmail.com>
2019-04-22 11:59:28 -0500

    front-end bootstrap project copied

---
+++ vue-training/src/components/Example.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  p.example
+    | This is {{ name }}.
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue single file component'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.example {
+  display: block;
+  padding-top: 30px;
+  text-align: center;
+}
+</style>
